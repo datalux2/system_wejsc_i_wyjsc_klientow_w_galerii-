eedf82d50a138d3ca217a69a3337f76ae03f30a5
Tomasz Figiel <datalux9@gmail.com>
2022-09-18 19:38:07 +0200

    zmiany

--- app/Models/InputsOutputsHoursModel.php
+++ app/Models/InputsOutputsHoursModel.php
@@ -13,60 +13,81 @@
         }
         
         public function agregate()
+        {  
+           $date_today = date('Y-m-d');
+           
+           $hour_today = date('H');
+           
+           $date_yesterday = date('Y-m-d', strtotime($date_today . ' ' . $hour_today . ':00:00') - 60 * 60);
+           
+           $hour_yesterday = date('H', strtotime($date_today . ' ' . $hour_today . ':00:00') - 60 * 60);
+           
+           if (!empty($this->if_datetime_exists($date_yesterday, $hour_yesterday)))
+           {
+                $this->agregate_db($date_yesterday, $hour_yesterday);
+           }
+           
+           if (!empty($this->if_datetime_exists($date_today, $hour_today)))
+           {
+                $this->agregate_db($date_today, $hour_today); 
+           }
+        }
+        
+        public function if_datetime_exists($date, $hour)
         {
            $builder_inputs_outputs = $this->db->table('inputs_outputs');
            
-           $builder_inputs_outputs_hours = $this->db->table('inputs_outputs_hours');
-           
-           $builder_inputs_outputs->select('DATE(datetime) date, HOUR(datetime) hour');
-           
-           $builder_inputs_outputs->distinct();
+           $builder_inputs_outputs->where('date(datetime)', $date);
+           $builder_inputs_outputs->where('hour(datetime)', $hour);
            
            $query = $builder_inputs_outputs->get();
            
            $result = $query->getResultArray();
            
-           if (!empty($result))
-           {
-               foreach ($result as $row)
-               {
-                   $builder_inputs_outputs_hours->where('date', $row['date']);
-                   $builder_inputs_outputs_hours->where('hour', $row['hour']);
-                   $query2 = $builder_inputs_outputs_hours->get();
-                   
-                   $result2 = $query2->getRowArray();
-                   
-                   if(empty($result2))
-                   {   
-                        $result3 = $this->get_count_input_output($row['date'], $row['hour'], 1);
-                       
-                        $result4 = $this->get_count_input_output($row['date'], $row['hour'], 0);
-                              
-                        $data = [
-                             'date' => $row['date'],
-                             'hour'  => $row['hour'],
-                             'input'  => $result3['count_input'],
-                             'output' => $result4['count_output']
-                         ];
+           return $result;
+        }
+        
+        public function agregate_db($date, $hour)
+        {
+            $builder_inputs_outputs = $this->db->table('inputs_outputs');
+           
+            $builder_inputs_outputs_hours = $this->db->table('inputs_outputs_hours');
+            
+            $builder_inputs_outputs_hours->where('date', $date);
+            $builder_inputs_outputs_hours->where('hour', $hour);
+            $query2 = $builder_inputs_outputs_hours->get();
 
-                         $builder_inputs_outputs_hours->insert($data);
-                   }
-                   else
-                   {
-                       $result3 = $this->get_count_input_output($result2['date'], $result2['hour'], 1);
-                       
-                       $result4 = $this->get_count_input_output($result2['date'], $result2['hour'], 0);
-                       
-                        $data = [
-                             'input' => $result3['count_input'],
-                             'output'  => $result4['count_output']
-                        ];
-                        $builder_inputs_outputs_hours->where('date', $result2['date']);
-                        $builder_inputs_outputs_hours->where('hour', $result2['hour']);
-                        $builder_inputs_outputs_hours->update($data);
-                   }
-               }
-           }
+            $result2 = $query2->getRowArray();
+
+            if(empty($result2))
+            {   
+                 $result3 = $this->get_count_input_output($date, $hour, 1);
+
+                 $result4 = $this->get_count_input_output($date, $hour, 0);
+
+                 $data = [
+                      'date' => $date,
+                      'hour'  => $hour,
+                      'input'  => $result3['count_input'],
+                      'output' => $result4['count_output']
+                  ];
+
+                  $builder_inputs_outputs_hours->insert($data);
+            }
+            else
+            {
+                $result3 = $this->get_count_input_output($result2['date'], $result2['hour'], 1);
+
+                $result4 = $this->get_count_input_output($result2['date'], $result2['hour'], 0);
+
+                 $data = [
+                      'input' => $result3['count_input'],
+                      'output'  => $result4['count_output']
+                 ];
+                 $builder_inputs_outputs_hours->where('date', $result2['date']);
+                 $builder_inputs_outputs_hours->where('hour', $result2['hour']);
+                 $builder_inputs_outputs_hours->update($data);
+            }
         }
         
         public function get_count_persons()
